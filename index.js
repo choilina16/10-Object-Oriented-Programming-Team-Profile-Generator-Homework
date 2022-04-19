d6f666e46a9909f6bfbdfabcb189fd676c783e10
Lina Choi <choilina16@gmail.com>
2022-04-18 23:19:39 -0400

    update

--- index.js
+++ index.js
@@ -1,57 +1,94 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Employee = require('./lib/Employee');
+// const inquirer = require('inqurier');
+// const fs = require('fs');
 
-const managerInit = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Enter the team manager name',
-            },
-            {
-                type: 'input',
-                name: 'id',
-                message: 'Enter the team manager employee ID',
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter the team manager email address',
-            },
-            {
-                type: 'input',
-                name: 'username',
-                message: 'Enter the team manager office number',
-            },
-        ])
-        .then(res => {
-            console.log(res);
-            const myManager = new Employee(
-                res.name, 
-                res.id, 
-                res.email, 
-                res.username
-            );
-            console.log(myManager);
-            // instantiate a new object and pass in the values from the user 
-            myManager.sayEmployeeValues();
+// const generateHTML = (answers) =>
+//   `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Team Builder</title>
 
-            inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'goAgain',
-                    message: 'Do you want to make a new employee?'
-                }
-            ]).then(confirm => {
-                if(confirm.goAgain) {
-                    init();
-                } else {
-                    process.exit();
-                }
-            })
-        });
-};
+//     <!--Import materialize.css-->
+//     <!-- USING MATERIALIZE FRAMEWORK!! -->
+//     <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+//     <!-- custom css file -->
+//     <link rel="stylesheet" href="./css/style.css">
+// </head>
 
-managerInit();
+// <body>
+
+//     <!-- header/navbar -->
+//     <div class="navbar-fixed">
+//         <nav>
+//         <div class="nav-wrapper">
+//             <a href="#!" class="brand-logo">Team Builder</a>
+//         </div>
+//         </nav>
+//     </div>
+
+//     <!-- Manager -->
+//     <div class="row">
+//         <div class="col s12 m6">
+//         <div class="card blue-grey darken-1">
+//             <div class="card-content white-text">
+//             <span class="card-title">Manager</span>
+//             <ul>
+//                 <li>Name</li>
+//                 <li>Employee ID</li>
+//                 <li>Email</li>
+//                 <li>Office Number</li>
+//             </ul>
+//             </div>
+//         </div>
+//         </div>
+//     </div>
+
+//     <!-- Engineer -->
+//     <div class="row">
+//     <div class="col s12 m6">
+//         <div class="card blue-grey darken-1">
+//         <div class="card-content white-text">
+//             <span class="card-title">Engineer</span>
+//             <ul>
+//             <li>Name</li>
+//             <li>Employee ID</li>
+//             <li>Email</li>
+//             <li>Github Username</li>
+//             </ul>
+//         </div>
+//         </div>
+//     </div>
+//     </div>
+
+
+//     <!-- Intern -->
+//     <div class="row">
+//         <div class="col s12 m6">
+//         <div class="card blue-grey darken-1">
+//             <div class="card-content white-text">
+//             <span class="card-title">Intern</span>
+//             <ul>
+//                 <li>Name</li>
+//                 <li>Employee ID</li>
+//                 <li>Email</li>
+//                 <li>School</li>
+//             </ul>
+//             </div>
+//         </div>
+//         </div>
+//     </div>
+    
+// </body>
+// </html>`;
+
+// .then((answers) => {
+//     const htmlPageContent = generateHTML(answers);
+
+//     // this is actually writing the file. if errors then it will tell you so in console log
+//     fs.writeFile('index.html', htmlPageContent, (err) =>
+//       err ? console.log(err) : console.log('Successfully created index.html!')
+//     );
+//   });
+
